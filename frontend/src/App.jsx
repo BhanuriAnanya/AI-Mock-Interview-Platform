b7f17133866eb4bd0ac2987d29e747abb047c277
BhanuriAnanya <ananya.appy@gmail.com>
2026-08-12 22:32:13 +0530

    Fix case-sensitive Result import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Result from "./pages/Result";
+import Result from "./pages/result";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
